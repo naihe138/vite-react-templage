8f2cafd19e8bdaf1457a4e864b4582d669ffbbda
naihe138 <370215230@qq.com>
2022-04-13 21:52:09 +0800

    init

---
+++ src/layouts/index.tsx
@@ -0,0 +1,29 @@
+import { Outlet, Link, useNavigate } from 'react-router-dom';
+const Layout = () => {
+  const navigate = useNavigate();
+
+  return (
+    <div>
+      <h1>layout</h1>
+      <nav>
+        <ul>
+          <li>
+            <Link to="/">Home</Link>
+          </li>
+          <li>
+            <Link to="/about">about</Link>
+          </li>
+          <li>
+            <Link to="/xxx">Nothing Here</Link>
+          </li>
+          <li onClick={() => navigate('/about')}>click to about</li>
+          <li onClick={() => navigate(-1)}>click to about</li>
+        </ul>
+      </nav>
+      <hr />
+      <Outlet />
+    </div>
+  );
+};
+
+export default Layout;
