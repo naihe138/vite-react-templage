a5fb9414ffa49e8a2087cdb3b9a9f9828d230816
naihe138 <370215230@qq.com>
2022-04-15 21:26:38 +0800

    docs: readme

--- src/main.tsx
+++ src/main.tsx
@@ -1,14 +1,18 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import '@/styles/reset.scss';
+import { BrowserRouter } from 'react-router-dom';
+import AppRouters from './routes';
 import './index.css';
-import App from './App';
+import '~/styles/reset.scss';
+
 import { RecoilRoot } from 'recoil';
 
 ReactDOM.render(
   <React.StrictMode>
     <RecoilRoot>
-      <App />
+      <BrowserRouter>
+        <AppRouters />
+      </BrowserRouter>
     </RecoilRoot>
   </React.StrictMode>,
   document.getElementById('root'),
